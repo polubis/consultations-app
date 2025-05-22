5668d724556a4dde116e5d37bfe94e5f4a08126b
polubis <polubis@wp.pl>
2025-05-22 11:59:43 +0200

    fix(app): cache for home
    fix(app): home page
    
    fix(app): disable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  output: "server",
   adapter: cloudflare(),
   site: "https://consultations-app-production.pages.dev",
   devToolbar: {
